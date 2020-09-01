22f38176c412bc5c6e3ac819aa9e0b6b2d268a2e
Rahul Sabharwal <sabharwalrahul2000@gmail.com>
2020-09-01 13:32:47 +0530

    Updates

---
+++ precache-manifest.ac052609823ac29f0c63b806c7466b56.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95a125d4c5efc7d2a26488a56d851c55",
+    "url": "/CoMarch/index.html"
+  },
+  {
+    "revision": "85d576934e7246683eda",
+    "url": "/CoMarch/static/css/main.89035580.chunk.css"
+  },
+  {
+    "revision": "cdc9f501767effdd9477",
+    "url": "/CoMarch/static/js/2.ed712804.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/CoMarch/static/js/2.ed712804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85d576934e7246683eda",
+    "url": "/CoMarch/static/js/main.4ec0f7b0.chunk.js"
+  },
+  {
+    "revision": "51fab5128639d81888a8",
+    "url": "/CoMarch/static/js/runtime-main.f111a7cb.js"
+  },
+  {
+    "revision": "1b943d724cb2d7c49f888f750ce3a479",
+    "url": "/CoMarch/static/media/google.1b943d72.png"
+  },
+  {
+    "revision": "97647e53329ba91f2679869bfe4b9b3c",
+    "url": "/CoMarch/static/media/ui.97647e53.png"
+  }
+]);
